cd09c2262d93df05654c4a672f314615aa1ca18e
lucas-d00 <reposnate1738@gmail.com>
2022-09-15 14:39:57 -0300

    se agrego react-router-dom y layout base para el Catálogo

---
+++ src/pages/Catalog.tsx
@@ -0,0 +1,15 @@
+
+function Catalog() {
+    return(
+        <div className="Catalog">
+            <div className="catalog-header">
+                <h1>Productos</h1>
+            </div>
+            <div className="catalog-body">
+                <p>(Grilla de productos)</p>
+            </div>
+        </div>
+    )
+}
+
+export default Catalog;
