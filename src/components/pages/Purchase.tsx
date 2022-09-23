8f1ca19444d7e9a600fadb412d9ca2d70cf66037
lucas-d00 <reposnate1738@gmail.com>
2022-09-23 20:09:54 -0300

    Se agrego selector de tipo de envio y se desplega el formulario para ingresar la dirección

--- src/components/pages/Purchase.tsx
+++ src/components/pages/Purchase.tsx
@@ -1,17 +1,47 @@
-import { Col, Container, Row } from 'react-bootstrap'
+import { useState } from 'react'
+import { Col, Container, FormCheck, FormControl, FormLabel, Row } from 'react-bootstrap'
 
 const Purchase = () => {
+  const [tipoDeEnvio, setTipoDeEnvio] = useState('')
+
   return (
     <div className='PurchasePage'>
       <div className='PurchaseContainer'>
         <Container>
           <Row>
             <Col> Datos del comprador </Col>
-            <Col> Datos de envío </Col>
+            <Col>
+              <p>Datos de envío</p>
+              <div>
+                {tipoDeEnvio == 'Envio' ? (
+                  <div>
+                    <FormLabel>Dirección de envío:</FormLabel>
+                    <FormControl type='text' />
+                  </div>
+                ) : (
+                  <div></div>
+                )}
+              </div>
+            </Col>
             <Col> Datos de pago </Col>
           </Row>
           <Row>
-            <Col> Selección de envío </Col>
+            <Col>
+              <FormCheck
+                type='radio'
+                id='inline-radio'
+                name='group1'
+                label='Retirar producto'
+                onChange={() => setTipoDeEnvio('Retiro')}
+              />
+              <FormCheck
+                type='radio'
+                id='inline-radio'
+                name='group1'
+                label='Enviar producto'
+                onChange={() => setTipoDeEnvio('Envio')}
+              />
+            </Col>
             <Col> Selección de pago </Col>
             <Col> Pagar </Col>
           </Row>
