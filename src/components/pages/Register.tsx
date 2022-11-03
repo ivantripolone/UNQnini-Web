6b1418613a3312b50d9fecc964c4da785fb71372
lucas-d00 <reposnate1738@gmail.com>
2022-11-02 21:22:41 -0300

    Se agrego el formulario y boton de registro

--- src/components/pages/Register.tsx
+++ src/components/pages/Register.tsx
@@ -1,6 +1,30 @@
+import { FormControl, FormGroup } from "react-bootstrap"
+import registerbutton from '../../assets/Register_Button.png'
 
 const Register = () => {
-    return (<div></div>)
+
+    const register = () => {
+
+    }
+
+    return (
+        <div className='d-flex flex-column registerContainer'>
+            <div className='registerFields'>
+                <FormGroup >
+                    <FormControl type='text' placeholder='Nombre de usuario' />
+                    <FormControl type='password' placeholder='Contraseña' />
+                    <FormControl type='text' placeholder='Nombre y Apellido' />
+                    <FormControl type='text' placeholder='CUIT' />
+                    <FormControl type='text' placeholder='Nombre del comercio' />
+                    <FormControl type='text' placeholder='Dirección del comercio' />
+                    <FormControl type='text' placeholder='URL de Imagen' />
+                </FormGroup>
+            </div>
+            <div className='registerButton'>
+                <button id='BotonRegistrarse' onClick={register}> <img src={registerbutton} alt='' /></button>
+            </div>
+        </div>
+    )
 }
 
 export default Register
